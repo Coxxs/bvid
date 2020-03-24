f848b19042650228ffc9b4114cb44674b64dc163
Coxxs <Coxxs@users.noreply.github.com>
2020-03-24 12:35:21 +0800

    fix variable declaration

--- bvid.js
+++ bvid.js
@@ -1,16 +1,16 @@
-var bvid = (function () {
-  var table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
-  tr = {}
-  for (var i = 0; i < 58; i++) {
+const bvid = (function () {
+  let table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
+  let tr = {}
+  for (let i = 0; i < 58; i++) {
     tr[table[i]] = i
   }
-  var s = [11, 10, 3, 8, 4, 6, 2, 9, 5, 7]
-  var xor = BigInt('177451812')
-  var add = BigInt('100618342136696320')
+  let s = [11, 10, 3, 8, 4, 6, 2, 9, 5, 7]
+  let xor = BigInt('177451812')
+  let add = BigInt('100618342136696320')
 
   function decode(x) {
-    r = BigInt(0)
-    for (var i = 0; i < 10; i++) {
+    let r = BigInt(0)
+    for (let i = 0; i < 10; i++) {
       r += BigInt(tr[x[s[i]]]) * (BigInt(58) ** BigInt(i))
     }
     r = ((r - add) ^ xor)
@@ -23,8 +23,8 @@ var bvid = (function () {
       return null
     }
     x = (BigInt(x) ^ xor) + add
-    r = ['B', 'V']
-    for (var i = 0; i < 10; i++) {
+    let r = ['B', 'V']
+    for (let i = 0; i < 10; i++) {
       r[s[i]] = table[x / BigInt(58) ** BigInt(i) % BigInt(58)]
     }
     return r.join('')
